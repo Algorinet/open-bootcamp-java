b2df16f426716099f4bf12545933d8477c98fcf6
Alberto <algorinetportfolio@gmail.com>
2023-01-11 01:33:56 +0100

    ejercicio 1

---
+++ src/main/java/Ejercicios/Ejercicio1.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int age = 38;
+        String name = "Alberto";
+        Boolean studing = true;
+        long puk = 2345788;
+        double note = 7.3;
+
+        System.out.println("El estudiante " + name + " y edad " + age + " ha sacado una nota de: " + note + ". Se le asigna el siguiente código: " + puk + " y su estado cambia a " + studing);
+    }
+}
